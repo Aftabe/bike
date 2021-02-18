e528d483ae2ca04bdfad45d998ca04f485742cfd
Aftabe <shamsasghari77@gmail.com>
2021-02-18 21:52:41 +1100

    bck color has been changed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.body.style.backgroundColor = 'red';
+document.body.style.backgroundColor = 'blue';
 document.body.style.color = 'white';
 document.body.style.fontFamily = 'arial';
 document.body.style.fontSize = '30px';
